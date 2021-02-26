0cdf11410423d40a94851ef742587ab4a3220f26
Bogdan Opanchuk <bogdan@opanchuk.net>
2021-02-25 18:38:08 -0800

    Add experiment with decoherence test

--- test_experiment.py
+++ test_experiment.py
@@ -105,5 +105,50 @@ def test_experiment():
             path="figures/experiment_" + ('cpu' if gpu_id is None else 'gpu'))
 
 
+def test_experiment_dc():
+
+    ensembles = 120
+    samples_per_ensemble = 1000
+    dc = 0.12
+
+    system = System(
+        unitary=transmission_matrix(),
+        inputs=50,
+        squeezing=squeezing_coefficients(),
+        input_transmission=1 / (1 + dc)**0.5,
+        thermal_noise=dc * numpy.sinh(squeezing_coefficients())**2,
+        )
+
+    for gpu_id in (None, 2,):
+        merged_result_set = simulate_sequential(
+            system=system,
+            ensembles=ensembles,
+            samples_per_ensemble=samples_per_ensemble,
+            measurements=dict(
+                population=Measure(Population()),
+                moments=Measure(Moments(5), stages={'out'}),
+                click_probability=Measure(ClickProbability(), stages={'out'}, representations={Representation.POSITIVE_P}),
+                clicks=Measure(Clicks(5), stages={'out'}, representations={Representation.POSITIVE_P}),
+                zero_clicks=Measure(ZeroClicks(5), stages={'out'}, representations={Representation.POSITIVE_P}),
+                compound_click_probability=Measure(CompoundClickProbability(100), stages={'out'}, representations={Representation.POSITIVE_P})
+                ),
+            gpu_id=gpu_id)
+
+        add_reference_experiment(system, merged_result_set)
+
+        plot_results(
+            merged_result_set,
+            tags=dict(
+                population={'lin', 'errors'},
+                moments={'log', 'errors'},
+                click_probability={'lin', 'errors'},
+                clicks={'log', 'errors'},
+                zero_clicks={'log', 'errors'},
+                compound_click_probability={'lin', 'log', 'errors'}
+            ),
+            path="figures/experiment_dc_" + ('cpu' if gpu_id is None else 'gpu'))
+
+
 if __name__ == '__main__':
     test_experiment()
+    test_experiment_dc()
