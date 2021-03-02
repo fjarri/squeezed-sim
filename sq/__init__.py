f73a300762212c124a139f4d7a0b1c48419ae38e
Bogdan Opanchuk <bogdan@opanchuk.net>
2021-03-01 20:40:44 -0800

    Export more stuff from the root

--- sq/__init__.py
+++ sq/__init__.py
@@ -1,4 +1,4 @@
-from .simulate import simulate_sequential, Measure
-from .meters_cpu import Population, Moments, ClickProbability, Clicks, ZeroClicks, CompoundClickProbability
+from .simulate import simulate_sequential, Measure, make_seed
+from .meters_cpu import Population, Moments, ClickProbability, Clicks, ZeroClicks, CompoundClickProbability, nchooseft
 from .system import System, Representation
 from .plot import plot_results
